ecf966dea14650a21eee1b6aeb833794e31f7c1f
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-07 15:53:26 -0700

    ready for test deployment

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,77 @@
-const express = require('express')
+const express = require("express");
 
-const router =  express.Router()
+const router = express.Router();
 
-module.exports = router
+const User = require("../models/user");
+
+// Getting all
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch {
+    res.status(500).json({message: err.message});
+  }
+});
+
+// Getting One
+router.get("/:id", getUser, (req, res) => {
+  res.json(res.user);
+});
+
+// Creating one
+router.post("/", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    avatar: req.body.avatar,
+  });
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({message: err.message});
+  }
+});
+
+// Updating One
+router.patch("/:id", getUser, async (req, res) => {
+  if (req.body.name != null) {
+    res.user.name = req.body.name;
+  }
+  if (req.body.avatar != null) {
+    res.user.avatar = req.body.avatar;
+  }
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({message: err.message});
+  }
+});
+
+// Deleting One
+router.delete("/:id", getUser, async (req, res) => {
+  try {
+    await res.user.remove();
+    res.json({message: "deleted user"});
+  } catch {
+    res.send(500).json({message: err.message});
+  }
+});
+
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({message: "Cannot find User"});
+    }
+  } catch {
+    return res.status(500).json({message: err.message});
+  }
+
+  res.user = user;
+  next();
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors")
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true, })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+mongoose.connect(process.env.DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
-app.use(express.json())
+app.use(express.json());
 
-const userRouter = require('./routes/userRoutes')
-app.use('/user', userRouter)
+const userRouter = require("./routes/userRoutes");
+app.use("/user", userRouter);
 
-app.listen(3001, () => console.log('Server Started'))
+app.listen(3001, () => console.log("Server Started"));
