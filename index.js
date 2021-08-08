c9b64ecae5820cfd69a106bb63ea51cd2a8de4bd
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-08 11:19:24 -0700

    added welcome to the api message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors")
+const server = express();
 const port = process.env.PORT || 3001;
 app.use(
   cors({
@@ -22,5 +23,13 @@ app.use(express.json());
 
 const userRouter = require("./routes/userRoutes");
 app.use("/user", userRouter);
+
+server.get("/", (req, res) => {
+	res.json({
+		message: "Welcome to the API",
+	});
+});
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}`))
