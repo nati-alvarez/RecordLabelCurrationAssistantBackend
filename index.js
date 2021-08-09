816e2e69681e447deea6eae1e7881279d5d425dd
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-09 16:13:39 -0700

    converted var to array

--- index.js
+++ index.js
@@ -34,24 +34,16 @@ app.get("/", (req, res) => {
 
 //get Request Token
 
-let discogsAccessData
+const discogsAccessData = []
 
 app.get("/authorize", (req, res) => {
   var oAuth = new Discogs().oauth();
   oAuth.getRequestToken(
     process.env.DISCOGS_API_KEY,
     process.env.DISCOGS_API_SECRET,
-    "https://rlca-backend.herokuapp.com/callback",
+    "http://localhost:3001/callback",
     function (err, requestData) {
-      // Persist "requestData" here so that the callback handler can
-      // access it later after returning from the authorize url
-      // if (typeof localStorage === "undefined" || localStorage === null) {
-      //   var LocalStorage = require("node-localstorage").LocalStorage;
-      //   localStorage = new LocalStorage("./scratch");
-      // }
-      // const serializedRequestData = JSON.stringify(requestData);
-      // localStorage.setItem("request", serializedRequestData);
-      discogsAccessData = requestData
+      discogsAccessData.push(requestData) 
       res.redirect(requestData.authorizeUrl);
     }
   );
@@ -60,44 +52,31 @@ app.get("/authorize", (req, res) => {
 // get access token
 
 app.get("/callback", (req, res) => {
-  // if (typeof localStorage === "undefined" || localStorage === null) {
-  //   var LocalStorage = require("node-localstorage").LocalStorage;
-  //   localStorage = new LocalStorage("./scratch");
-  // }
-  // let deserializedRequestData = JSON.parse(localStorage.getItem("request"));
-  var oAuth = new Discogs(discogsAccessData).oauth();
+  var oAuth = new Discogs(discogsAccessData[0]).oauth();
   oAuth.getAccessToken(
-    req.query.oauth_verifier, // Verification code sent back by Discogs
+    req.query.oauth_verifier,
     function (err, accessData) {
-      // Persist "accessData" here for following OAuth calls
-      // const serializedAccessData = JSON.stringify(accessData);
-      // localStorage.setItem("access", serializedAccessData);
-      discogsAccessData = accessData
-      res.redirect('http://localhost:3000/dashboard');
-      // res.send("Received access token!");
+      discogsAccessData.push(accessData)
+      res.send("Received access token!");
     }
   );
-
 });
 
 // make the OAuth call
 
-app.get("/identity", (req, res) => {
-  // if (typeof localStorage === "undefined" || localStorage === null) {
-  //   var LocalStorage = require("node-localstorage").LocalStorage;
-  //   localStorage = new LocalStorage("./scratch");
-  // }
-  // let deserializedAccessData = JSON.parse(localStorage.getItem("access"));
-
-  var dis = new Discogs('Sonic Arch', discogsAccessData);
-  res.json(dis);
-  console.log(dis)
-  res.send(dis)
-  dis.getIdentity(function (err, data) {
-    console.log(data)
-    res.send(data);
-  });
-  res.redirect('http://localhost:3000/dashboard');
+app.get('/identity', function(req, res){
+  console.log(discogsAccessData)
+  var dis = new Discogs(discogsAccessData[1]);
+	dis.getIdentity(function(err, data){
+		res.send(data);
+	});
 });
+
+
+// discogs test call
+
+
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}`));
