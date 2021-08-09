4e2fb3509d24532d06651d28759c86bc6da294b3
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-08 18:13:16 -0700

    added discogs auth

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const Discogs = require('disconnect').Client;
+const Discogs = require("disconnect").Client;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors")
+const cors = require("cors");
 
 const port = process.env.PORT || 3001;
 app.use(
@@ -24,84 +24,69 @@ app.use(express.json());
 
 const userRouter = require("./routes/userRoutes");
 
-
 app.use("/user", userRouter);
 
-
 app.get("/", (req, res) => {
-	res.json({
-		message: "Welcome to the API",
-	});
+  res.json({
+    message: "Welcome to the API",
+  });
 });
-
-
 
 //get Request Token
 
-
-app.get('/authorize', (req, res) => {
-	var oAuth = new Discogs().oauth();
-	oAuth.getRequestToken(
-		process.env.DISCOGS_API_KEY, 
-		process.env.DISCOGS_API_SECRET, 
-		'http://localhost:3000/dashboard', 
-		function(err, requestData){
-            
-			// Persist "requestData" here so that the callback handler can 
-			// access it later after returning from the authorize url
+app.get("/authorize", (req, res) => {
+  var oAuth = new Discogs().oauth();
+  oAuth.getRequestToken(
+    process.env.DISCOGS_API_KEY,
+    process.env.DISCOGS_API_SECRET,
+    "http://localhost:3000/dashboard",
+    function (err, requestData) {
+      // Persist "requestData" here so that the callback handler can
+      // access it later after returning from the authorize url
       if (typeof localStorage === "undefined" || localStorage === null) {
-        var LocalStorage = require('node-localstorage').LocalStorage;
-        localStorage = new LocalStorage('./scratch');
+        var LocalStorage = require("node-localstorage").LocalStorage;
+        localStorage = new LocalStorage("./scratch");
       }
-            const serializedRequestData = JSON.stringify(requestData)
-            localStorage.setItem("request", serializedRequestData)
+      const serializedRequestData = JSON.stringify(requestData);
+      localStorage.setItem("request", serializedRequestData);
 
-			res.redirect(requestData.authorizeUrl);
-		}
-	);
+      res.redirect(requestData.authorizeUrl);
+    }
+  );
 });
-
-
 
 // get access token
 
-app.get('/callback', (req, res) => {
-	if (typeof localStorage === "undefined" || localStorage === null) {
-        var LocalStorage = require('node-localstorage').LocalStorage;
-        localStorage = new LocalStorage('./scratch');
-      }
-    let deserializedRequestData = JSON.parse(localStorage.getItem("request"))
-	var oAuth = new Discogs(deserializedRequestData).oauth();
-	oAuth.getAccessToken(
-		req.query.oauth_verifier, // Verification code sent back by Discogs
-		function(err, accessData){
-
-
-
-			// Persist "accessData" here for following OAuth calls 
-            const serializedAccessData = JSON.stringify(accessData)
-            localStorage.setItem("access", serializedAccessData)
-			res.send('Received access token!');
-		}
-	);
+app.get("/callback", (req, res) => {
+  if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require("node-localstorage").LocalStorage;
+    localStorage = new LocalStorage("./scratch");
+  }
+  let deserializedRequestData = JSON.parse(localStorage.getItem("request"));
+  var oAuth = new Discogs(deserializedRequestData).oauth();
+  oAuth.getAccessToken(
+    req.query.oauth_verifier, // Verification code sent back by Discogs
+    function (err, accessData) {
+      // Persist "accessData" here for following OAuth calls
+      const serializedAccessData = JSON.stringify(accessData);
+      localStorage.setItem("access", serializedAccessData);
+      res.send("Received access token!");
+    }
+  );
 });
-
 
 // make the OAuth call
 
-app.get('/identity', (req, res) => {
-	if (typeof localStorage === "undefined" || localStorage === null) {
-        var LocalStorage = require('node-localstorage').LocalStorage;
-        localStorage = new LocalStorage('./scratch');
-      }
-    let deserializedAccessData = JSON.parse(localStorage.getItem("access"))
-	var dis = new Discogs(deserializedAccessData);
-	dis.getIdentity(function(err, data){
-		res.send(data);
-	});
+app.get("/identity", (req, res) => {
+  if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require("node-localstorage").LocalStorage;
+    localStorage = new LocalStorage("./scratch");
+  }
+  let deserializedAccessData = JSON.parse(localStorage.getItem("access"));
+  var dis = new Discogs(deserializedAccessData);
+  dis.getIdentity(function (err, data) {
+    res.send(data);
+  });
 });
 
-
-
-
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`));
