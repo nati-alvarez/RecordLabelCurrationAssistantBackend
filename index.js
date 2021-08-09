d09d206086a76a4866bed3ccb0ebf1c2975c24c0
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-08 17:14:12 -0700

    imported local storage package

--- index.js
+++ index.js
@@ -66,16 +66,17 @@ app.get('/authorize', (req, res) => {
 // get access token
 
 app.get('/callback', (req, res) => {
+	if (typeof localStorage === "undefined" || localStorage === null) {
+        var LocalStorage = require('node-localstorage').LocalStorage;
+        localStorage = new LocalStorage('./scratch');
+      }
     let deserializedRequestData = JSON.parse(localStorage.getItem("request"))
 	var oAuth = new Discogs(deserializedRequestData).oauth();
 	oAuth.getAccessToken(
 		req.query.oauth_verifier, // Verification code sent back by Discogs
 		function(err, accessData){
 
-      if (typeof localStorage === "undefined" || localStorage === null) {
-        var LocalStorage = require('node-localstorage').LocalStorage;
-        localStorage = new LocalStorage('./scratch');
-      }
+
 
 			// Persist "accessData" here for following OAuth calls 
             const serializedAccessData = JSON.stringify(accessData)
@@ -89,6 +90,10 @@ app.get('/callback', (req, res) => {
 // make the OAuth call
 
 app.get('/identity', (req, res) => {
+	if (typeof localStorage === "undefined" || localStorage === null) {
+        var LocalStorage = require('node-localstorage').LocalStorage;
+        localStorage = new LocalStorage('./scratch');
+      }
     let deserializedAccessData = JSON.parse(localStorage.getItem("access"))
 	var dis = new Discogs(deserializedAccessData);
 	dis.getIdentity(function(err, data){
