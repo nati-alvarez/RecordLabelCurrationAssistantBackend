f1d2d2effd01c824f9800ac491214c3b3524b3ce
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-29 11:40:54 -0700

    changes secure to hardcoded true

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: false,
       path: "/",
       httpOnly: true,
