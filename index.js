8fba5e2e22bc1c9e0dd87657faab1b1d1e9d1d38
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-08-30 12:22:50 -0700

    new debug branch

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const session = require("express-session");
+const session = require("express-session");
 const port = process.env.PORT || 3001;
 const cookieParser = require("cookie-parser");
-let cookieSession = require("cookie-session");
+// let cookieSession = require("cookie-session");
 
 app.use(cookieParser());
 
@@ -21,56 +21,51 @@ app.use(
   })
 );
 
-// app.use(function(req, res, next) {
-// res.setHeader(
-//  "Access-Control-Allow-Credentials", "true",
-// )
-// next()
-// })
-// app.use(
-//   session({
-//     secret: "keyboard cat",
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//       secure: process.env.NODE_ENV === "production",
-//       sameSite: false,
-//       path: "/",
-//       httpOnly: true,
-//     },
-//     maxAge: 24 * 60 * 60 * 1000 * 100, // 2400 hours
-//   })
-// );
-app.set("trust proxy", 1); // trust first proxy
+app.use(function(req, res, next) {
+res.setHeader(
+ "Access-Control-Allow-Credentials", "true",
+)
+next()
+})
+
+//WITH SESSION
 app.use(
-  cookieSession({
-    name: "session",
-    secret: "secret",
-    secure: process.env.NODE_ENV === "production",
-    // Cookie Options
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: false,
+      path: "/",
+      httpOnly: true,
+    },
     maxAge: 24 * 60 * 60 * 1000 * 100, // 2400 hours
   })
 );
+app.set("trust proxy", 1); // trust first proxy
+
+//WITH COOKIES
+
+// app.use(
+//   cookieSession({
+//     name: "session",
+//     secret: "secret",
+//     secure: process.env.NODE_ENV === "production",
+//     // Cookie Options
+//     maxAge: 24 * 60 * 60 * 1000 * 100, // 2400 hours
+//   })
+// );
+
+//DB CONNECTION
 
 mongoose.connect(process.env.DATABASE_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
 const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Database"));
 
-app.use(express.json());
-
-const userRouter = require("./routes/userRoutes");
-
-app.use("/user", userRouter);
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to the API",
-  });
-});
+// URL'S
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
@@ -81,7 +76,25 @@ const client_url =
     ? "https://sonic-architecture-v1.netlify.app"
     : "http://localhost:3000";
 
-const tempArr = [];
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+app.use(express.json());
+
+const userRouter = require("./routes/userRoutes");
+
+app.use("/user", userRouter);
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the API",
+  });
+});
+
+
+
+
 //get Request Token
 app.get("/authorize", (req, res) => {
   let oAuth = new Discogs().oauth();
@@ -89,10 +102,8 @@ app.get("/authorize", (req, res) => {
     process.env.DISCOGS_API_KEY,
     process.env.DISCOGS_API_SECRET,
     `${API_BASE_URL}/callback`,
-    // "http://localhost:3001/callback",
     function (err, requestData) {
       req.session.requestData = JSON.stringify(requestData);
-      tempArr.push(requestData);
       res.redirect(requestData.authorizeUrl);
     }
   );
@@ -101,22 +112,17 @@ app.get("/authorize", (req, res) => {
 // get access token
 
 app.get("/callback", (req, res) => {
-  let oAuth = new Discogs(tempArr[0]).oauth();
-  // let oAuth = new Discogs(JSON.parse(req.session.requestData)).oauth();
+  let oAuth = new Discogs(JSON.parse(req.session.requestData)).oauth();
   oAuth.getAccessToken(req.query.oauth_verifier, function (err, accessData) {
     req.session.accessData = JSON.stringify(accessData);
-    tempArr.push(accessData);
     res.redirect(`${client_url}/authorizing`);
-    // res.redirect("http://localhost:3000/authorizing");
   });
 });
 
 // make the OAuth call
 
 app.get("/identity", function (req, res) {
-  let dis = new Discogs(tempArr[1]);
-  // let dis = new Discogs(JSON.parse(req.session.accessData));
-  console.log(tempArr[1]);
+  let dis = new Discogs(JSON.parse(req.session.accessData));
   dis.getIdentity(function (err, data) {
     console.log(err, data);
     res.send(data);
@@ -128,8 +134,7 @@ app.get("/identity", function (req, res) {
 app.get("/search", function (req, res) {
   let dis = new Discogs(
     "Sonic Archtecturev1.0",
-    tempArr[1]
-    // JSON.parse(req.session.accessData)
+    JSON.parse(req.session.accessData)
   );
   dis.database().search(req.query.discogsAccessparams, function (err, data) {
     console.log(err, data);
@@ -141,8 +146,20 @@ app.get("/search", function (req, res) {
 app.get("/usersLabelsSearch", function (req, res) {
   let dis = new Discogs(
     "Sonic Archtecturev1.0",
-    tempArr[1]
-    // JSON.parse(req.session.accessData)
+    JSON.parse(req.session.accessData)
+  );
+  dis
+    .database()
+    .getLabelReleases(req.query.discogsAccessParams, function (err, data) {
+      console.log(err);
+      res.send(data);
+    });
+});
+
+app.get("/usersReleasesSearch", function (req, res) {
+  let dis = new Discogs(
+    "Sonic Archtecturev1.0",
+    JSON.parse(req.session.accessData)
   );
   dis
     .database()
